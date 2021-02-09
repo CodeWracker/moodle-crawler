fa170c53987668bf50c48b8f68d6e52e0baadb62
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-02-09 18:19:35 -0300

    fiz um pouco do design das configurações

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import React, { useState, useEffect } from "react";
+import "react-awesome-button/dist/themes/theme-c137.css";
 import "./App.css";
 import "./customButtons.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCogs } from "@fortawesome/free-solid-svg-icons";
 import Home from "./components/Home/Home";
 import Settings from "./components/Settings/Settings";
+import { AwesomeButton } from "react-awesome-button";
 
 function App() {
-  const [pagina, setPagina] = useState("home");
+  const [pagina, setPagina] = useState("Tarefas");
 
   function handleConfigButton() {
-    "home" === pagina ? setPagina("settings") : setPagina("home");
+    "Tarefas" === pagina ? setPagina("Configurações") : setPagina("Tarefas");
   }
 
   return (
     <div className="App">
       <div className="header-bar">
-        <a
-          className="btn-config"
-          onClick={() => {
-            handleConfigButton();
-          }}
-        >
-          <FontAwesomeIcon icon={faCogs}></FontAwesomeIcon>
-        </a>
-        <a className="btn-atualizar">Atualizar</a>
+        <div className="functions">
+          <div className="btn-container">
+            <AwesomeButton
+              className="btn aws-btn"
+              size="icon"
+              type="primary"
+              onPress={() => {
+                handleConfigButton();
+              }}
+            >
+              <FontAwesomeIcon icon={faCogs}></FontAwesomeIcon>
+            </AwesomeButton>
+          </div>
+
+          <div className="btn-container">
+            <AwesomeButton className="btn" type="secondary" onPress={() => {}}>
+              Atualizar
+            </AwesomeButton>
+          </div>
+        </div>
+        <p className="page-title">{pagina}</p>
       </div>
       <div className="body-content">
-        {"home" === pagina ? <Home></Home> : <Settings></Settings>}
+        {"Tarefas" === pagina ? <Home></Home> : <Settings></Settings>}
       </div>
     </div>
   );
--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from "react";
 import "./Settings.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCogs } from "@fortawesome/free-solid-svg-icons";
+import { faAt, faKey } from "@fortawesome/free-solid-svg-icons";
 
 function Settings() {
-  return <div className="container">settings</div>;
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+  return (
+    <div className="container">
+      <div className="moodle-config-container">
+        <p className="configTitle">Credenciais Moodle</p>
+        <div className="infos-input">
+          <div className="input-container">
+            <FontAwesomeIcon icon={faAt}></FontAwesomeIcon>
+            <input
+              className="my-input"
+              placeholder={login ? login : "Email ou CPF"}
+            ></input>
+          </div>
+          <div className="input-container">
+            <FontAwesomeIcon icon={faKey}></FontAwesomeIcon>
+            <input
+              className="my-input"
+              placeholder={senha ? senha : "Senha"}
+            ></input>
+          </div>
+        </div>
+      </div>
+      <div className="crawler-config-container">
+        <p className="configTitle">Configurações do robô</p>
+        <div className="materia-config">
+          <p className="meteria-title">(20202) - Cálculo III</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Settings;
