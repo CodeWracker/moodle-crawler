f6c22c994eec0826a0e281296549b037fe876d92
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-02-09 14:13:12 -0300

    Adicionei a troca de tela

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "./customButtons.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCogs } from "@fortawesome/free-solid-svg-icons";
 import Home from "./components/Home/Home";
+import Settings from "./components/Settings/Settings";
 
 function App() {
+  const [pagina, setPagina] = useState("home");
+
+  function handleConfigButton() {
+    "home" === pagina ? setPagina("settings") : setPagina("home");
+  }
+
   return (
     <div className="App">
       <div className="header-bar">
-        <a className="btn-config">
+        <a
+          className="btn-config"
+          onClick={() => {
+            handleConfigButton();
+          }}
+        >
           <FontAwesomeIcon icon={faCogs}></FontAwesomeIcon>
         </a>
         <a className="btn-atualizar">Atualizar</a>
       </div>
       <div className="body-content">
-        <Home></Home>
+        {"home" === pagina ? <Home></Home> : <Settings></Settings>}
       </div>
     </div>
   );
---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./Settings.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCogs } from "@fortawesome/free-solid-svg-icons";
+
+function Settings() {
+  return <div className="container">settings</div>;
+}
+
+export default Settings;
